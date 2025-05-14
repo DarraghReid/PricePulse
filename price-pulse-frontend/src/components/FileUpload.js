5bd4f5eed249cbaf98452c4736ce7cbf2500bc36
Darragh Reid <darragh.reid@gmail.com>
2025-05-14 08:53:24 +0100

    Enhance file upload UI with validation, loading indicator, and clear input

--- price-pulse-frontend/src/components/FileUpload.js
+++ price-pulse-frontend/src/components/FileUpload.js
@@ -1,16 +1,26 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { ClipLoader } from 'react-spinners';
 
 function FileUpload({ setOptimizedPrices }) {
   const [file, setFile] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   // Handle file selection
   const onFileChange = (event) => {
+    const uploadedFile = event.target.files[0];
+    if (uploadedFile && uploadedFile.type !== 'text/csv') {
+        alert('Please upload a valid CSV file.');
+        setFile(null);
+        setLoading(false)
+        return;
+    }
     setFile(event.target.files[0]);
   };
 
   // Handle file upload to backend
   const onFileUpload = async () => {
+    setLoading(true);
     const formData = new FormData();
     formData.append('file', file);
 
@@ -24,13 +34,27 @@ function FileUpload({ setOptimizedPrices }) {
     } catch (error) {
       console.error('There was an error uploading the file!', error);
     }
+    setLoading(false);
   };
 
   return (
+    
     <div>
-      <h2>Upload Retail Data</h2>
+        {loading && (
+            <div className="loader">
+            <ClipLoader color="#36d7b7" loading={loading} size={50} />
+            </div>
+        )}
+        {!loading && (
+            <div>
+            <h2>Upload Retail Data</h2>
+            <input type="file" onChange={onFileChange} />
+            <button onClick={onFileUpload}>Upload and Optimize Prices</button>
+            </div>
+        )}
+      {/* <h2>Upload Retail Data</h2>
       <input type="file" onChange={onFileChange} />
-      <button onClick={onFileUpload}>Upload and Optimize Prices</button>
+      <button onClick={onFileUpload}>Upload and Optimize Prices</button> */}
     </div>
   );
 }
